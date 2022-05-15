fa0034ae794762d98bba29c2a1010fa4f091ec30
Kamila Vieira <vkamila54@outlook.com>
2022-05-14 23:41:09 -0300

    server: Ajustes para deploy

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,11 +3,12 @@ import cors from "cors";
 import { routes } from "./routes";
 
 const app = express();
+const PORT = process.env.PORT || 3333;
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
-  console.log("HTTP server running on http://localhost:3333");
+app.listen(PORT, () => {
+  console.log(`HTTP server running on http://localhost:${PORT}`);
 });
