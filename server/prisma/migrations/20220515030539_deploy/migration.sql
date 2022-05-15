9995c018167e20d32690ade2f485a298104148e4
Kamila Vieira <vkamila54@outlook.com>
2022-05-15 00:06:56 -0300

    server: Atualizando migration prisma

--- server/prisma/migrations/20220515030539_deploy/migration.sql
+++ server/prisma/migrations/20220515030539_deploy/migration.sql
@@ -1,7 +1,9 @@
 -- CreateTable
 CREATE TABLE "feedbacks" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "type" TEXT NOT NULL,
     "comment" TEXT NOT NULL,
-    "screenshot" TEXT
+    "screenshot" TEXT,
+
+    CONSTRAINT "feedbacks_pkey" PRIMARY KEY ("id")
 );
